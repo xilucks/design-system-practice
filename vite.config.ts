740c3b9aee90da0d1c3dac167e08d3f54d06945a
Choi siun <siun0331@gmail.com>
2024-02-05 10:49:56 +0900

    feat: radix primitive 활용한 checkbox 예제 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-/// <reference types="vitest" />
 import { join, resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-
-import { peerDependencies } from './package.json';
 
 export default defineConfig({
   plugins: [
@@ -12,17 +9,13 @@ export default defineConfig({
     dts({ rollupTypes: true }), // Output .d.ts files
   ],
   build: {
-    target: 'esnext',
-    minify: false,
     lib: {
       entry: resolve(__dirname, join('lib', 'index.ts')),
       fileName: 'index',
       formats: ['es', 'cjs'],
     },
-    rollupOptions: {
-      // Exclude peer dependencies from the bundle to reduce bundle size
-      external: ['react/jsx-runtime', ...Object.keys(peerDependencies)],
-    },
+    minify: false,
+    target: 'esnext',
   },
   test: {
     environment: 'jsdom',

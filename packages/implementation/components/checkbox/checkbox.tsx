111a4d738295d174c6cef4d04eaa2d546a6aadd0
Choi siun <siun0331@gmail.com>
2024-02-05 18:16:05 +0900

    feat: checkbox 예제 스토리북 코드 추가

--- packages/implementation/components/checkbox/checkbox.tsx
+++ packages/implementation/components/checkbox/checkbox.tsx
@@ -1,15 +1,35 @@
+// Checkbox.tsx
+
+'use client';
+
 import * as CheckBoxPrimitive from '@/core/primitives/checkbox/checkbox-primitive.tsx';
 import type { ReactNode } from 'react';
+import { useState } from 'react';
+import './checkbox.scss';
 
 interface CheckboxProps {
+  onCheckedChange?: () => void;
   labelText: ReactNode;
 }
 
-export default function Checkbox({ labelText }: CheckboxProps) {
+export default function Checkbox({ onCheckedChange = () => {}, labelText }: CheckboxProps) {
+  const [checked, setChecked] = useState<boolean>(false);
   return (
-    <CheckBoxPrimitive.Root id={'check-box-1'}>
-      <CheckBoxPrimitive.Indicator>checked!</CheckBoxPrimitive.Indicator>
-      <label htmlFor={'check-box-1'}>{labelText}</label>
-    </CheckBoxPrimitive.Root>
+    <div className="checkbox-container">
+      <CheckBoxPrimitive.Root
+        className="checkbox-root"
+        id="check-box-1"
+        checked={checked}
+        onCheckedChange={() => {
+          setChecked(!checked);
+          onCheckedChange();
+        }}
+      >
+        <CheckBoxPrimitive.Indicator className="checkbox-indicator">
+          check!
+        </CheckBoxPrimitive.Indicator>
+      </CheckBoxPrimitive.Root>
+      <label htmlFor="check-box-1">{labelText}</label>
+    </div>
   );
 }

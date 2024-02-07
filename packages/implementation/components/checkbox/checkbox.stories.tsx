cddf8941a9b7e940b1e41c3cb9226305e3777113
최시운 책임/GQMS 엔지니어 <siun.choi@hyundai-autoever.com>
2024-02-07 13:13:22 +0900

    feat: 기타 수정

--- packages/implementation/components/checkbox/checkbox.stories.tsx
+++ packages/implementation/components/checkbox/checkbox.stories.tsx
@@ -1,60 +1,60 @@
-import React, { useState } from "react";
-import Checkbox from "./checkbox";
-import Button from "../button";
-import type { Meta, StoryObj } from "@storybook/react";
-import Flex from "../../layouts/flex/flex";
+import React, { useState } from 'react';
+import Checkbox from './checkbox';
+import Button from '../button/Button';
+import type { Meta, StoryObj } from '@storybook/react';
+import Flex from '../../layouts/flex/flex';
 
 const meta = {
-  title: "implementation/checkbox",
+  title: 'implementation/checkbox',
   component: Checkbox,
   parameters: {
-    layout: "centered",
-    componentSubtitle: "checkbox",
+    layout: 'centered',
+    componentSubtitle: 'checkbox',
     docs: {
       description: {
-        component: "checkbox는 체크 여부를 판단하는 컴포넌트 입니다.",
+        component: 'checkbox는 체크 여부를 판단하는 컴포넌트 입니다.',
       },
     },
   },
   argTypes: {
     defaultChecked: {
-      description: "최초 체크가 되어있는지의 여부를 지정합니다.",
+      description: '최초 체크가 되어있는지의 여부를 지정합니다.',
       control: {
-        type: "boolean",
+        type: 'boolean',
       },
     },
     disabled: {
-      description: "체크박스의 사용 가능 여부를 지정합니다.",
+      description: '체크박스의 사용 가능 여부를 지정합니다.',
       control: {
-        type: "boolean",
+        type: 'boolean',
       },
     },
     required: {
-      description: "필수적으로 체크되어야 하는 지에 대한 여부를 지정합니다.",
+      description: '필수적으로 체크되어야 하는 지에 대한 여부를 지정합니다.',
       control: {
-        type: "boolean",
+        type: 'boolean',
       },
     },
     name: {
-      description: "checkbox의 name을 지정합니다.",
+      description: 'checkbox의 name을 지정합니다.',
       control: {
-        type: "text",
+        type: 'text',
       },
     },
     value: {
-      description: "checkbox의 value을 지정합니다.",
+      description: 'checkbox의 value을 지정합니다.',
       control: {
-        type: "text",
+        type: 'text',
       },
     },
     labelText: {
-      description: "checkbox의 label에 들어갈 text를 지정합니다.",
+      description: 'checkbox의 label에 들어갈 text를 지정합니다.',
       control: {
-        type: "text",
+        type: 'text',
       },
     },
   },
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 } satisfies Meta<typeof Checkbox>;
 
 export default meta;
@@ -65,9 +65,9 @@ const RequiredTemplate = (args) => {
 
   const handleSubmit = () => {
     if (args.require && !isChecked) {
-      alert("체크박스를 선택해주세요!");
+      alert('체크박스를 선택해주세요!');
     } else {
-      alert("폼 제출 성공!");
+      alert('폼 제출 성공!');
     }
   };
 
@@ -80,7 +80,10 @@ const RequiredTemplate = (args) => {
         onChange={() => setIsChecked(!isChecked)}
         required={args.require}
       />
-      <Button type="submit" onClick={handleSubmit}>
+      <Button
+        type="submit"
+        onClick={handleSubmit}
+      >
         제출
       </Button>
     </Flex>
@@ -89,24 +92,24 @@ const RequiredTemplate = (args) => {
 
 export const DefaultTextbox = Template.bind({});
 DefaultTextbox.args = {
-  name: "default-checkbox",
-  value: "checkbox1",
-  labelText: "Default Checkbox",
+  name: 'default-checkbox',
+  value: 'checkbox1',
+  labelText: 'Default Checkbox',
 };
 
 export const DisabledTextbox = Template.bind({});
 DisabledTextbox.args = {
-  name: "disabled-checkbox",
-  value: "checkbox1",
-  labelText: "Disabled Checkbox",
+  name: 'disabled-checkbox',
+  value: 'checkbox1',
+  labelText: 'Disabled Checkbox',
   disabled: true,
 };
 
 export const RequiredTextbox = RequiredTemplate.bind({});
 RequiredTextbox.args = {
-  name: "required-checkbox",
-  value: "checkbox1",
-  labelText: "Required CheckBox",
+  name: 'required-checkbox',
+  value: 'checkbox1',
+  labelText: 'Required CheckBox',
   require: true,
   onChange: () => {},
 };

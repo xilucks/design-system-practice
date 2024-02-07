b11e27f3cf85a88b3989118127e9e644108b0c24
Choi siun <siun0331@gmail.com>
2024-02-07 14:41:45 +0900

    feat: 배포를 위한 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dfs from 'vite-plugin-dts';
 
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), dfs()],
   build: {
+    lib: {
+      entry: {
+        components: path.resolve(__dirname, './packages/implementation/components/index.ts'),
+      },
+      name: 'uni.choi-DesignSystem',
+      fileName: (format) => `index.${format}.js`,
+    },
     outDir: 'dist',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
   },
 });
